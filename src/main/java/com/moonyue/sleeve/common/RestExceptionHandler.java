f611f366afb7e7730c4e9112ae6906ab096dbda0
luna825 <luna825@qq.com>
2020-07-12 22:54:19 +0800

    添加dto校验异常全局处理

--- src/main/java/com/moonyue/sleeve/common/RestExceptionHandler.java
+++ src/main/java/com/moonyue/sleeve/common/RestExceptionHandler.java
@@ -6,12 +6,22 @@ import com.moonyue.sleeve.common.configuration.CodeMessageConfiguration;
 import com.moonyue.sleeve.core.bean.Code;
 import com.moonyue.sleeve.core.exception.HttpException;
 import com.moonyue.sleeve.vo.UnifyResponseVO;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.net.BindException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.moonyue.sleeve.common.util.RequestUtil.getSimpleRequest;
 
@@ -47,6 +57,30 @@ public class RestExceptionHandler {
         }else{
             result.setMessage(errorMessage);
         }
+        return result;
+    }
+
+    /*
+    *MethodArgumentNotValidException
+    * */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public UnifyResponseVO processException(MethodArgumentNotValidException exception, HttpServletRequest request, HttpServletResponse response){
+        BindingResult bindResult = exception.getBindingResult();
+        List<ObjectError> errors = bindResult.getAllErrors();
+        Map<String, Object> msg = new HashMap<>();
+        UnifyResponseVO result = new UnifyResponseVO();
+        errors.forEach(error ->{
+            if(error instanceof FieldError){
+                FieldError fieldError = (FieldError) error;
+                msg.put(StrUtil.toUnderlineCase(fieldError.getField()), fieldError.getDefaultMessage());
+            }else{
+                msg.put(StrUtil.toUnderlineCase(error.getObjectName()), error.getDefaultMessage());
+            }
+        });
+        result.setRequest(getSimpleRequest(request));
+        result.setMessage(msg);
+        result.setCode(Code.PARAMETER_ERROR.getCode());
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
         return result;
     }
 }
